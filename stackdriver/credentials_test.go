4d8682db9308d6e1fd2b8d2da33b191035571566
Ben Hale <bhale@pivotal.io>
2020-03-30 10:31:54 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- stackdriver/credentials_test.go
+++ stackdriver/credentials_test.go
@@ -47,6 +47,11 @@ func testCredentials(t *testing.T, context spec.G, it spec.S) {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
+	it.After(func() {
+		Expect(os.RemoveAll(ctx.Buildpack.Path)).To(Succeed())
+		Expect(os.RemoveAll(ctx.Layers.Path)).To(Succeed())
+	})
+
 	it("contributes Properties", func() {
 		Expect(os.MkdirAll(filepath.Join(ctx.Buildpack.Path, "bin"), 0755)).To(Succeed())
 		Expect(ioutil.WriteFile(filepath.Join(ctx.Buildpack.Path, "bin", "google-application-credentials"), []byte{}, 0644)).
