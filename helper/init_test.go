1ccc50783ad4da2d0c6daf3dc1cc1ba0d412267a
Ben Hale <bhale@vmware.com>
2020-08-24 20:53:16 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- helper/init_test.go
+++ helper/init_test.go
@@ -24,7 +24,7 @@ import (
 )
 
 func TestUnit(t *testing.T) {
-	suite := spec.New("credentials", spec.Report(report.Terminal{}))
+	suite := spec.New("helper", spec.Report(report.Terminal{}))
 	suite("Credentials", testCredentials)
 	suite("JavaDebugger", testJavaDebugger)
 	suite("JavaProfiler", testJavaProfiler)
